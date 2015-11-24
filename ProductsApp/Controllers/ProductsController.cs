b94fc566e1bbcc85e0716ce60424a03403537ced
gomeznegrin <gomeznegrin.uk@gmail.com>
2015-11-24 21:46:56 +0000

    Initial import

---
+++ ProductsApp/Controllers/ProductsController.cs
@@ -0,0 +1,36 @@
+﻿using ProductsApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace ProductsApp.Controllers
+{
+    public class ProductsController : ApiController
+    {
+
+        Product[] products = new Product[]
+        {
+            new Product { Id = 1, Name = "Sopita de tomate", Category = "Sopitas", Price = 10 },
+            new Product { Id = 2, Name = "Bocadillo de mierda", Category = "PretAMangui", Price = 3.75M },
+            new Product { Id = 3, Name = "Martillo", Category = "Herramientas", Price = 16.99M }
+        };
+
+        public IEnumerable<Product> GetAllProducts()
+        {
+            return products;
+        }
+
+        public IHttpActionResult GetProduct(int id)
+        {
+            var product = products.FirstOrDefault((p) => p.Id == id);
+            if (product == null)
+            {
+                return NotFound();
+            }
+            return Ok(product);
+        }
+    }
+}
